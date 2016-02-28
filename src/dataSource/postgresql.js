27d31afd24c32fe8560bf488a4c5b746fb0b2c79
leocavalcante <lc@leocavalcante.com>
2016-02-28 13:24:52 -0300

    Makes PGSQL data source match the API interface

--- src/dataSource/postgresql.js
+++ src/dataSource/postgresql.js
@@ -1,5 +1,19 @@
 import pgp from 'pg-promise';
 
+const apiInterface = data => {
+  if (data.created_at) {
+    data.createdAt = data.created_at;
+    delete data.created_at;
+  }
+
+  if (data.contact_user) {
+    data.isContact = data.contact_user;
+    delete data.contact_user;
+  }
+
+  return data;
+}
+
 export default function (dsn) {
   if (!dsn) {
     throw new Error('Using postgresql requires DSN/Connection string defined as PSQL_DSN on Node.js env.');
@@ -62,19 +76,32 @@ export default function (dsn) {
            WHERE l.created_by = $1
         `, id),
         db.query(`
-          SELECT a.id,
-                 a.created_at,
-                 a.cover_letter
-            FROM applications a
-           WHERE a.user_id = $1
+             SELECT a.id,
+                    a.created_at,
+                    a.cover_letter,
+                    l.id listing_id,
+                    l.name listing_name,
+                    l.description listing_description
+               FROM applications a
+          LEFT JOIN listings l ON l.id = a.listing_id
+              WHERE a.user_id = $1
         `, id)
       ]);
 
       return Object.assign(
-        results[0], {
-          companies: results[1],
-          createdListings: results[2],
-          applications: results[3]
+        apiInterface(results[0]), {
+          companies: results[1].map(apiInterface),
+          createdListings: results[2].map(apiInterface),
+          applications: results[3].map(application => ({
+            id: application.id,
+            createdAt: application.created_at,
+            listing: {
+              id: application.listing_id,
+              name: application.listing_name,
+              description: application.listing_description
+            },
+            coverLetter: application.cover_letter
+          }))
         }
       );
     }
